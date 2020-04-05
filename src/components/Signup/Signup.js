c5117662bd3ad44622bbd9b98a63609fc29ca2e8
Ken Austria <kenaustriadev@gmail.com>
2020-04-05 00:12:31 -0700

    Signup Form Draft

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -8,12 +8,12 @@ export const Signup = () => {
 				<p className='header-button-text'><span>Try it free 7 days</span> then $20/mo. thereafter</p>
 			</div>
 			<div className='form'>
-				<input type='text' className='input' />
-				<input type='text' className='input' />
-				<input type='text' className='input' />
-				<input type='text' className='input' />
+				<div className='field'>
+					<input type='text' className='input' />
+					<p className='error'>First Name cannot be empty.</p>
+				</div>
 				<button>Claim Your Free Trial</button>
-				<p>By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
+				<p>By clicking the button, you are agreeing to our <span className='legal'>Terms and Services</span></p>
 			</div>
 		</div>
 	)
